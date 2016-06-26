3bf947674938377b8d8dab96253d4a17626d2dd9
shahmalav <malavpshah19@gmail.com>
2016-06-26 15:13:38 -0700

    Add adapter for listview

--- app/src/main/java/com/shahmalav/androidprojects/sunshine/MainActivity.java
+++ app/src/main/java/com/shahmalav/androidprojects/sunshine/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View rootView =inflater.inflate(R.layout.fragment_main, container);
+
+            View rootView =inflater.inflate(R.layout.fragment_main, container, false);
 
             List<String> mockList = new ArrayList<String>();
             mockList.add("Today - Sunny - 88/63");
@@ -37,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
             mockList.add("Thurs - Rainy - 64/51");
             mockList.add("Fri - Foggy - 70/46");
             mockList.add("Sat - Sunny - 76/68");
+
+            ArrayAdapter adapter = new ArrayAdapter(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,mockList);
+            ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
+            listView.setAdapter(adapter);
 
             return super.onCreateView(inflater, container, savedInstanceState);
         }
